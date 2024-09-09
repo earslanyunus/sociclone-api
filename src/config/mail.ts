062354f88d3c09c8dd4b4e33fa89572e022d97f6
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2024-09-10 01:47:05 +0300

    mail text updated

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -6,8 +6,8 @@ export const sendOTPEmail = async (to: string, otp: string) => {
     const { data, error } = await resend.emails.send({
         from: process.env.MAIL_USER as string,
         to: to,
-        subject: 'Hesap Doğrulama - OTP',
-        html: `Merhaba, kaydınızı tamamlamak için OTP kodunuz: ${otp}.`
+        subject: 'OTP Verification',
+        html: `Your OTP code is: ${otp}.`
           });
     
       if (error) {
